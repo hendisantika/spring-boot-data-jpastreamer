7a5d7c2dfb5297ca42e600f509096c8b4238d379
hendisantika <hendisantika@yahoo.co.id>
2023-03-13 09:08:57 +0700

    #4 - Add data-h2.sql file ✏️📗:octocat:

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,23 @@
+INSERT INTO author
+VALUES (1, 'Joanne', 'Rowling')
+    INSERT
+INTO author
+VALUES (2, 'William', 'Shakespeare')
+INSERT INTO author
+VALUES (3, 'James', 'Patterson')
+INSERT INTO author
+VALUES (4, 'Mark', 'Twain')
+INSERT INTO author
+VALUES (5, 'Leo', 'Tolstoy')
+
+INSERT INTO book
+VALUES (1, 'The Casual Vacancy', 1)
+INSERT INTO book
+VALUES (2, 'The Cuckcoos Calling', 1)
+INSERT INTO book
+VALUES (3, 'Harry Potter and Chamber of Secrets', 1)
+INSERT INTO book
+VALUES (4, 'Harry Potter and Sorcerers stone', 1)
+INSERT INTO book
+VALUES (5, 'Harry Potter and the half blood Prince', 1)
+
