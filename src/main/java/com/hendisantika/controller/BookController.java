052faf3acbc54c7de32c720c8285e875eefa0587
hendisantika <hendisantika@yahoo.co.id>
2023-03-13 09:22:33 +0700

    #9 - Add findBookIds in BookController ✏️📗:octocat:

--- src/main/java/com/hendisantika/controller/BookController.java
+++ src/main/java/com/hendisantika/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,12 +28,19 @@ public class BookController {
 
     private final JPAStreamer jpaStreamer;
 
-    @GetMapping("/books")
+    @GetMapping
     public ResponseEntity<?> findAllBooks() {
         return ResponseEntity.ok(
                 jpaStreamer.stream(Book.class)
                         .sorted(Book$.id.reversed())
                         .collect(Collectors.toList()));
+    }
+
+    @GetMapping("/ids")
+    public ResponseEntity<?> findBookIds() {
+        final LongStream ids = jpaStreamer.stream(Book.class)
+                .mapToLong(Book$.id.asLong());
+        return ResponseEntity.ok(ids);
     }
 
 }
