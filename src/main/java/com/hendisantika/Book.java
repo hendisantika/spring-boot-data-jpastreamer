0c7824b1a9474cf1ac0e1412abb77f3e342ae71c
hendisantika <hendisantika@yahoo.co.id>
2023-03-13 09:11:22 +0700

    #6 - Add Book Entity ✏️📗:octocat:

--- src/main/java/com/hendisantika/Book.java
+++ src/main/java/com/hendisantika/Book.java
@@ -3,13 +3,14 @@ package com.hendisantika;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,24 +19,28 @@ import java.util.List;
  * Email: hendisantika@gmail.com
  * Telegram : @hendisantika34
  * Date: 3/13/23
- * Time: 09:09
+ * Time: 09:10
  * To change this template use File | Settings | File Templates.
  */
 @Getter
 @Setter
-@Entity(name = "author")
+@Entity(name = "book")
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Author {
-
+public class Book {
     @Id
     @GeneratedValue
     private Long id;
 
-    private String firstName;
-
     @Column(unique = true)
-    private String lastName;
+    private String title;
 
-    @OneToMany
-    private List<Book> books;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public Book(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
 }
