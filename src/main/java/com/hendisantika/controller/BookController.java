4560124e39a698ebaf1ef78e11a954121dc067dc
hendisantika <hendisantika@yahoo.co.id>
2023-03-13 09:22:08 +0700

    #8 - Add findAllBooks in BookController ✏️📗:octocat:

--- src/main/java/com/hendisantika/controller/BookController.java
+++ src/main/java/com/hendisantika/controller/BookController.java
@@ -1,9 +1,14 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.Book;
 import com.speedment.jpastreamer.application.JPAStreamer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,11 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 09:11
  * To change this template use File | Settings | File Templates.
  */
-@RequestMapping("/api")
+@RequestMapping("/api/books")
 @RestController
 @RequiredArgsConstructor
 public class BookController {
 
     private final JPAStreamer jpaStreamer;
+
+    @GetMapping("/books")
+    public ResponseEntity<?> findAllBooks() {
+        return ResponseEntity.ok(
+                jpaStreamer.stream(Book.class)
+                        .sorted(Book$.id.reversed())
+                        .collect(Collectors.toList()));
+    }
 
 }
